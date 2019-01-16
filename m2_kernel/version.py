f2706ffc72eb2cab014ca31b9dbb64921419129b
Radoslav <radoslav@math.cornell.edu>
2019-01-16 17:12:23 -0500

    minor - comments

--- m2_kernel/version.py
+++ m2_kernel/version.py
@@ -3,7 +3,7 @@
 Unlike most packages, __version__ can not simply be put in __init__.py
 The latter imports the kernel class, which in turn instantiates a whole bunch.
 To preserve single-source definition, we compromise with calling `exec`
-e.g. https://packaging.python.org/guides/single-sourcing-package-version
+for example, see https://packaging.python.org/guides/single-sourcing-package-version
 """
 
 __version__ = '0.1.1'
